c68de2518f25ce2f1f0b787fcf36f731ea1c1cbf
arley6mith <asreinas@unal.edu.co>
2024-03-28 11:15:59 -0500

    first try lab3

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,33 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    data = pd.read_fwf("clusters_report.txt", 
+                     header=None, 
+                     widths=[9, 16, 16, 77], 
+                     names=
+                     ["cluster",
+                      "cantidad_de_palabras_clave",
+                      "porcentaje_de_palabras_clave",
+                      "principales_palabras_clave" ],
+                     ).drop(range(0,3), axis=0).ffill()
+    
+    data.loc[:,"principales_palabras_clave"] = data["principales_palabras_clave"].str.replace(r'\s{2,}', ' ', regex=True)
+    data.loc[:,"principales_palabras_clave"] = data["principales_palabras_clave"].str.replace('.', '')
 
-    return df
+    data = data.groupby(
+        ["cluster",
+         "cantidad_de_palabras_clave",
+         "porcentaje_de_palabras_clave"], as_index=False).agg(
+             {'principales_palabras_clave': lambda x: ' '.join(x)}).reset_index()
+    
+    data.rename(columns={None: 'principales_palabras_clave'},inplace=True)
+    data.reset_index(drop=True, inplace=True)
+
+    data.loc[:,"cluster"] = data["cluster"].astype(int)
+    data.loc[:,"cantidad_de_palabras_clave"] = data["cantidad_de_palabras_clave"].astype(int)
+    data.loc[:,"porcentaje_de_palabras_clave"] = data["porcentaje_de_palabras_clave"].str.rstrip("%").str.replace(',','.').astype(float)
+    data.sort_values("cluster", inplace=True)
+   
+    return data
+
+#print(ingest_data().principales_palabras_clave.to_list()[0])
